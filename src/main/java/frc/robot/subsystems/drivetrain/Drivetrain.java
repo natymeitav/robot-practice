59bbad67037ed41067533125e9cdb25ac4ec7701
naty <naty.meitav@Gmail.com>
2020-07-31 12:33:02 +0300

    create Dtrivetrain

---
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -0,0 +1,36 @@
+package frc.robot.subsystems.drivetrain;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import frc.robot.Ports;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase {
+    public TalonFX mainR = new TalonFX(Ports.DriveTrain.mainR);
+    public TalonFX sideR = new TalonFX(Ports.DriveTrain.sideR);
+
+    public TalonFX mainL = new TalonFX(Ports.DriveTrain.mainL);
+    public TalonFX sideL = new TalonFX(Ports.DriveTrain.sideL);
+
+    public Drivetrain() {
+        sideR.follow(mainR);
+        mainR.setInverted(Ports.DriveTrain.invertedR);
+        sideL.follow(mainL);
+        mainL.setInverted(Ports.DriveTrain.invertedL);
+    }
+
+
+    public void setPowerR(double power) {
+        mainR.set(ControlMode.PercentOutput, power);
+    }
+
+    public void setPowerL(double power) {
+        mainL.set(ControlMode.PercentOutput, power);
+    }
+
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+}
