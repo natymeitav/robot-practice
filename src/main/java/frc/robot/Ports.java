a7c0ff1db8eb388f9048d470f57bfb6f502b1124
naty <naty.meitav@Gmail.com>
2020-07-31 12:45:54 +0300

    add ports

---
+++ src/main/java/frc/robot/Ports.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+public class Ports {
+    public class Intake{
+        public static final int Motor = 20;
+        public static final int Piston = 2;
+        public static final boolean inverted = false;
+    }
+
+    public class DriveTrain{
+        public static final int mainR = 12;
+        public static final int sideR = 13;
+        public static  final boolean invertedR = true;
+
+        public static final int mainL = 10;
+        public static final int sideL = 11;
+        public static final boolean invertedL = false;
+    }
+}
